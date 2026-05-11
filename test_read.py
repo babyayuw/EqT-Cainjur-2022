8832275fc5d07319fdd4bf8b86ce303791117132
babyayuw <babyayuw@gmail.com>
2026-05-11 08:34:37 +0700

    Delete test_read.py

--- test_read.py
+++
@@ -1,8 +0,0 @@
-from obspy import read
-
-# Test satu file dulu
-f = r"D:\BMKG\EQTransformer\sinyal gempa bandung 18 sept 2024\2024-09-18-02_41_07.mseed"
-print("Membaca file...")
-st = read(f)
-print(st)
-print("Berhasil!")
